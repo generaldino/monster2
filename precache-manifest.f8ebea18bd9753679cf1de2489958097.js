231733ef40efc451f1c60752622131a210b5cd80
generaldino <dannyhakim25@gmail.com>
2020-01-20 09:55:39 +0000

    Updates

---
+++ precache-manifest.f8ebea18bd9753679cf1de2489958097.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bfeecf592761936cb77f0e3533a4c099",
+    "url": "/monster2/index.html"
+  },
+  {
+    "revision": "e76642d5888cef883630",
+    "url": "/monster2/static/css/main.4b4bba82.chunk.css"
+  },
+  {
+    "revision": "c68e14fdc7e1ba4a5621",
+    "url": "/monster2/static/js/2.9e7d0785.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monster2/static/js/2.9e7d0785.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e76642d5888cef883630",
+    "url": "/monster2/static/js/main.8ae13692.chunk.js"
+  },
+  {
+    "revision": "b2aef54163d47e083932",
+    "url": "/monster2/static/js/runtime-main.4e1e8a13.js"
+  }
+]);
